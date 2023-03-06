436e7653ee60ad06a7e0661f20cfc2c1c47f511e
PiiaAro <piia.aro@gmail.com>
2023-03-06 19:22:01 +0200

    modified config.js api key

--- src/js/config.js
+++ src/js/config.js
@@ -4,5 +4,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = ' ';
+export const KEY = 'e3dd7f78-a835-436c-9229-5378a9486881';
 export const MODAL_CLOSE_SEC = 2.5;
